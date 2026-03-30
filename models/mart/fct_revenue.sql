bce6284e06fa5b893305213c62235157cc5e1350
Abhay Jamwal <abhaysinghjamwal2005@gmail.com>
2026-03-30 13:19:05 +0530

    fix: remove partition config from fct_revenue - BigQuery free tier limitation with small datasets

--- models/mart/fct_revenue.sql
+++ models/mart/fct_revenue.sql
@@ -1,15 +1,9 @@
 {{ config(
-    materialized = 'table',
-    partition_by = {
-        "field": "order_date",
-        "data_type": "date",
-        "granularity": "day"
-    },
-    cluster_by = ["user_id"]
+    materialized = 'table'
 ) }}
 
 WITH orders AS (
-    SELECT * 
+    SELECT *
     FROM {{ ref('int_orders_enriched') }}
 ),
 
@@ -19,22 +13,22 @@ refunds AS (
 ),
 
 joined AS (
-    SELECT  o.order_id, o.user_id, o.order_timestamp, DATE(o.order_timestamp) AS order_date,  o.order_amount,
+    SELECT
+        o.order_id,
+        o.user_id,
+        o.order_timestamp,
+        DATE(o.order_timestamp) AS order_date,
+        o.order_amount,
         o.currency,
         o.country,
         o.acquisition_channel,
-
         COALESCE(r.total_refunded_amount, 0) AS total_refund_amount,
-
         GREATEST(
             o.order_amount - COALESCE(r.total_refunded_amount, 0),
             CAST(0 AS NUMERIC)
         ) AS net_revenue
-
     FROM orders o
-    LEFT JOIN refunds r
-        ON o.order_id = r.order_id
+    LEFT JOIN refunds r ON o.order_id = r.order_id
 )
 
-SELECT *
-FROM joined
+SELECT * FROM joined
